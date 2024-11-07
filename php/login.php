cd02ec19efa4dbd3015d3008fbaaafccb6a4d666
Gowwwthami <mgowthami2955@gmail.com>
2024-11-07 19:02:01 +0530

    login.php added

--- php/login.php
+++ php/login.php
@@ -1,35 +1,17 @@
-<?php 
-
-
-session_start();
-include 'connection.php';
-
-if ($_SERVER["REQUEST_METHOD"]== "POST") {
-    
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $phone = $_POST['phone'];
-    $dob = $_POST['dob'];
-    $gender = $_POST['gender'];
-
-    $sql = "INSERT INTO `user`( `Name`, `Email`, `Password`, `Phone`, `age`, `gender`) VALUES (?,?,?,?,?,?)";
-    $stmt = $conn->prepare($sql);
-
-    if ($stmt) {
-        
-        $stmt->bind_param("ssssis",$name,$email, $password, $phone, $dob, $gender);
-        if ($stmt->execute()) {
-            $_SESSION['UserID'] = $stmt->insert_id; // Store UserID in session
-            header("../destinationType.html"); // Redirect to the next step
-            exit();
-        }else {
-            echo "Error: ". $stmt->error;
-        }
-        $stmt->close();
-    }
-    $conn->close();
-
-
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login page</title>
+</head>
+<body>
+    <form action="pracprob1.php" 
+    method="post">
+    <input type="text" placeholder="firstname" name="fname">
+    <br/>
+    <input type="password" placeholder="password" name="password"><br/>
+    <input type=submit>
+</form>
+</body>
+</html>
