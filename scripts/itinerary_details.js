f21e6f3a374fc83de6d32bf10d20108a11f2aaf5
Surwase Vinay Dnyaneshwar <146312926+VinaySurwase@users.noreply.github.com>
2024-11-29 17:32:45 +0530

    Modal class Commited

--- scripts/itinerary_details.js
+++ scripts/itinerary_details.js
@@ -1,48 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the modal and its elements
-    var modal = document.getElementById("myTripModal");
-    var span = document.querySelector("#myTripModal .close");
-    var modalTitle = document.getElementById("modal-title");
-    var modalDescription = document.getElementById("modal-description");
-    var modalFamous = document.getElementById("modal-famous");
-    var modalEntryFee = document.getElementById("modal-entry-fee");
-    var removeBtn = document.querySelector("#myTripModal .remove-btn");
+// document.addEventListener("DOMContentLoaded", function () {
+//     var modal = document.getElementById("myTripModal");
+//     var span = document.querySelector("#myTripModal .close");
+//     var modalTitle = document.getElementById("modal-title");
+//     var modalDescription = document.getElementById("modal-description");
+//     var modalFamous = document.getElementById("modal-famous");
+//     var modalEntryFee = document.getElementById("modal-entry-fee");
+//     var removeBtn = document.querySelector("#myTripModal .remove-btn");
 
-    // Get all cards
-    var cards = document.getElementsByClassName("card");
+//     var cards = document.getElementsByClassName("card");
 
-    // Loop through the cards and add click event listeners
-    for (var i = 0; i < cards.length; i++) {
-        cards[i].onclick = function () {
-            modalTitle.innerText = this.getAttribute("data-title");
-            modalDescription.innerText = this.getAttribute("data-description");
-            modalFamous.innerText = this.getAttribute("data-famous");
-            modalEntryFee.innerText = this.getAttribute("data-entry-fee");
-            modal.style.display = "block";
-        };
-    }
+//     for (var i = 0; i < cards.length; i++) {
+//         cards[i].onclick = function () {
+//             modalTitle.innerText = this.getAttribute("data-title");
+//             modalDescription.innerText = this.getAttribute("data-description");
+//             modalFamous.innerText = this.getAttribute("data-famous");
+//             modalEntryFee.innerText = this.getAttribute("data-entry-fee");
+//             modal.style.display = "block";
+//         };
+//     }
 
-    // Close the modal when the user clicks on <span> (x)
-    span.onclick = function () {
-        modal.style.display = "none";
-    };
+//     span.onclick = function () {
+//         modal.style.display = "none";
+//     };
 
-    // Close the modal when the user clicks outside it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
+//     window.onclick = function (event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     };
 
-    // Remove the current card when the remove button is clicked
-    removeBtn.onclick = function () {
-        var currentCardTitle = modalTitle.innerText;
-        for (var i = 0; i < cards.length; i++) {
-            if (cards[i].getAttribute("data-title") === currentCardTitle) {
-                cards[i].remove();
-                break;
-            }
-        }
-        modal.style.display = "none";
-    };
-});
+//     removeBtn.onclick = function () {
+//         var currentCardTitle = modalTitle.innerText;
+//         for (var i = 0; i < cards.length; i++) {
+//             if (cards[i].getAttribute("data-title") === currentCardTitle) {
+//                 cards[i].remove();
+//                 break;
+//             }
+//         }
+//         modal.style.display = "none";
+//     };
+// });
